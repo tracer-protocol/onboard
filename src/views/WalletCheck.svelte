0a5f78dfb547084852178284fd2dc08afd51b6b6
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2021-09-06 22:47:33 +1000

    Fix loading modal

--- src/views/WalletCheck.svelte
+++ src/views/WalletCheck.svelte
@@ -325,6 +325,12 @@
     align-items: center;
     margin-bottom: 1rem;
   }
+
+  .loading {
+    margin: 30px auto 20px;
+    width: 177px;
+    height: 92px;
+  }
 </style>
 
 {#if loadingModal}
@@ -335,51 +341,98 @@
 
 {#if activeModal}
   <Modal closeModal={() => handleExit()}>
-    <ModalHeader icon={activeModal.icon || ''} heading={activeModal.heading} />
-    <p class="bn-onboard-custom bn-onboard-prepare-description">
-      {@html activeModal.description}
-    </p>
-    {#if errorMsg}
-      <span
-        class:bn-onboard-dark-mode-background={$app.darkMode}
-        class="bn-onboard-custom bn-onboard-prepare-error"
-        in:fade
+    <div class="loading">
+      <svg
+        id="eaGBD5e1KEP1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="688 0 620 267.400000"
+        shape-rendering="geometricPrecision"
+        text-rendering="geometricPrecision"
       >
-        {errorMsg}
-      </span>
-    {/if}
-
-    {#if activeModal.html}
-      <section class="bn-onboard-custom bn-onboard-wallet-check-section">
-        {@html activeModal.html}
-      </section>
-    {/if}
-
-    <div class="bn-onboard-custom bn-onboard-prepare-button-container">
-      {#if activeModal.button}
-        <Button position="right" onclick={activeModal.button.onclick}>
-          {activeModal.button.text}
-        </Button>
-      {/if}
-      {#if errorMsg}
-        <Button onclick={doAction}>Try Again</Button>
-      {:else}
-        <div />
-      {/if}
-      {#if loading}
-        <Spinner />
-        {#if activeModal.getHelpLink}
-          <section class="bn-onboard-custom bn-onboard-get-help-container">
-            Having trouble?
-            <Button
-              help={true}
-              primary={true}
-              onclick={() => openLink(activeModal?.getHelpLink)}
-              >Get Help</Button
-            >
-          </section>
-        {/if}
-      {/if}
+        <style>
+          <![CDATA[#eaGBD5e1KEP4_to {animation: eaGBD5e1KEP4_to__to 1000ms linear infinite normal forwards}@keyframes eaGBD5e1KEP4_to__to { 0% {transform: translate(577.321785px,-59.500000px)} 50% {transform: translate(638.185892px,-59.500000px)} 100% {transform: translate(579.050000px,-59.500000px)}} #eaGBD5e1KEP6_to {animation: eaGBD5e1KEP6_to__to 1000ms linear infinite normal forwards}@keyframes eaGBD5e1KEP6_to__to { 0% {transform: translate(579.050000px,-59.500000px)} 50% {transform: translate(519.050000px,-59.500000px)} 100% {transform: translate(579.050000px,-59.500000px)}}]]>
+        </style>
+        <g id="eaGBD5e1KEP2">
+          <g id="eaGBD5e1KEP3" />
+          <g id="eaGBD5e1KEP4_to" transform="translate(577.321785,-59.500000)">
+            <polygon
+              id="eaGBD5e1KEP4"
+              points="667.700000,59.500000 436.100000,193.200000 667.700000,326.900000 667.700000,291.900000 496.900000,193.300000 667.700000,94.700000"
+              transform="translate(0,0)"
+              stroke="none"
+              stroke-width="1"
+            />
+          </g>
+          <rect
+            id="eaGBD5e1KEP5"
+            width="30.400000"
+            height="267.400000"
+            rx="0"
+            ry="0"
+            transform="matrix(1 0 0 1 984.75000000000000 0.00000000000003)"
+            stroke="none"
+            stroke-width="1"
+          />
+          <g id="eaGBD5e1KEP6_to" transform="translate(579.050000,-59.500000)">
+            <polygon
+              id="eaGBD5e1KEP6"
+              points="174.200000,94.700000 344.900000,193.300000 174.200000,291.900000 174.200000,326.900000 405.700000,193.200000 174.200000,59.500000"
+              transform="translate(0,0)"
+              stroke="none"
+              stroke-width="1"
+            />
+          </g>
+        </g>
+      </svg>
     </div>
+    <div>Confirm the prompt in your {activeModal.description} wallet.</div>
+
+    <!--    <ModalHeader icon={activeModal.icon || ''} heading={activeModal.heading} />-->
+    <!--    <p class="bn-onboard-custom bn-onboard-prepare-description">-->
+    <!--      {@html activeModal.description}-->
+    <!--    </p>-->
+    <!--    {#if errorMsg}-->
+    <!--      <span-->
+    <!--        class:bn-onboard-dark-mode-background={$app.darkMode}-->
+    <!--        class="bn-onboard-custom bn-onboard-prepare-error"-->
+    <!--        in:fade-->
+    <!--      >-->
+    <!--        {errorMsg}-->
+    <!--      </span>-->
+    <!--    {/if}-->
+
+    <!--    {#if activeModal.html}-->
+    <!--      <section class="bn-onboard-custom bn-onboard-wallet-check-section">-->
+    <!--        {@html activeModal.html}-->
+    <!--      </section>-->
+    <!--    {/if}-->
+
+    <!--    <div class="bn-onboard-custom bn-onboard-prepare-button-container">-->
+    <!--      {#if activeModal.button}-->
+    <!--        <Button position="right" onclick={activeModal.button.onclick}>-->
+    <!--          {activeModal.button.text}-->
+    <!--        </Button>-->
+    <!--      {/if}-->
+    <!--      {#if errorMsg}-->
+    <!--        <Button onclick={doAction}>Try Again</Button>-->
+    <!--      {:else}-->
+    <!--        <div />-->
+    <!--      {/if}-->
+    <!--      {#if loading}-->
+    <!--        <Spinner />-->
+    <!--        {#if activeModal.getHelpLink}-->
+    <!--          <section class="bn-onboard-custom bn-onboard-get-help-container">-->
+    <!--            Having trouble?-->
+    <!--            <Button-->
+    <!--              help={true}-->
+    <!--              primary={true}-->
+    <!--              onclick={() => openLink(activeModal?.getHelpLink)}-->
+    <!--              >Get Help</Button-->
+    <!--            >-->
+    <!--          </section>-->
+    <!--        {/if}-->
+    <!--      {/if}-->
+    <!--    </div>-->
   </Modal>
 {/if}
--- src/views/WalletSelect.svelte
+++ src/views/WalletSelect.svelte
@@ -349,6 +349,7 @@
     height: 10px;
     border-radius: 50%;
     background-color: #002886;
+    cursor: pointer;
   }
 
   .progress-indicator-selected {
@@ -361,8 +362,8 @@
 </style>
 
 {#if modalData}
-  {#if showConnection || localStorage.getItem('onboard.acceptedTerms') === 'true'}
-    <Modal closeModal={() => finish({ completed: false })}>
+  <Modal closeModal={() => finish({ completed: false })}>
+    {#if showConnection || localStorage.getItem('onboard.acceptedTerms') === 'true'}
       <ModalHeader icon={walletIcon} heading={modalData.heading} />
       {#if showTermsOfService}
         <p>
@@ -397,10 +398,15 @@
           {showAllWallets}
           {walletsDisabled}
         />
-        <div class="progress">
-          <div class="progress-indicator" />
-          <div class="progress-indicator-selected" />
-        </div>
+        {#if localStorage.getItem('onboard.acceptedTerms') !== 'true'}
+          <div class="progress">
+            <div
+              class="progress-indicator"
+              on:click={() => (showConnection = false)}
+            />
+            <div class="progress-indicator-selected" />
+          </div>
+        {/if}
         <div class="bn-onboard-custom bn-onboard-select-info-container">
           <span class="bn-onboard-custom bn-onboard-select-wallet-info">
             Are you new?
@@ -436,13 +442,20 @@
           {installMessage}
         />
       {/if}
-    </Modal>
-  {:else}
-    <Modal closeModal={() => finish({ completed: false })}>
+    {:else}
       <div class="terms-icon">
-        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect width="50" height="50" rx="25" fill="#D1FAE5"/>
-          <path d="M31.25 13.625C31.25 13.194 31.0788 12.7807 30.774 12.476C30.4693 12.1712 30.056 12 29.625 12C29.194 12 28.7807 12.1712 28.476 12.476C28.1712 12.7807 28 13.194 28 13.625V18.5H31.25V13.625ZM33.6875 20.125H15.8125C15.597 20.125 15.3903 20.2106 15.238 20.363C15.0856 20.5153 15 20.722 15 20.9375V22.5625C15 22.778 15.0856 22.9847 15.238 23.137C15.3903 23.2894 15.597 23.375 15.8125 23.375H16.625V25C16.6253 26.8729 17.2723 28.6882 18.4567 30.1391C19.641 31.5899 21.2901 32.5873 23.125 32.9625V38H26.375V32.9625C28.2099 32.5873 29.859 31.5899 31.0433 30.1391C32.2277 28.6882 32.8747 26.8729 32.875 25V23.375H33.6875C33.903 23.375 34.1097 23.2894 34.262 23.137C34.4144 22.9847 34.5 22.778 34.5 22.5625V20.9375C34.5 20.722 34.4144 20.5153 34.262 20.363C34.1097 20.2106 33.903 20.125 33.6875 20.125ZM21.5 13.625C21.5 13.194 21.3288 12.7807 21.024 12.476C20.7193 12.1712 20.306 12 19.875 12C19.444 12 19.0307 12.1712 18.726 12.476C18.4212 12.7807 18.25 13.194 18.25 13.625V18.5H21.5V13.625Z" fill="#0E9A6F"/>
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 50 50"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="50" height="50" rx="25" fill="#D1FAE5" />
+          <path
+            d="M31.25 13.625C31.25 13.194 31.0788 12.7807 30.774 12.476C30.4693 12.1712 30.056 12 29.625 12C29.194 12 28.7807 12.1712 28.476 12.476C28.1712 12.7807 28 13.194 28 13.625V18.5H31.25V13.625ZM33.6875 20.125H15.8125C15.597 20.125 15.3903 20.2106 15.238 20.363C15.0856 20.5153 15 20.722 15 20.9375V22.5625C15 22.778 15.0856 22.9847 15.238 23.137C15.3903 23.2894 15.597 23.375 15.8125 23.375H16.625V25C16.6253 26.8729 17.2723 28.6882 18.4567 30.1391C19.641 31.5899 21.2901 32.5873 23.125 32.9625V38H26.375V32.9625C28.2099 32.5873 29.859 31.5899 31.0433 30.1391C32.2277 28.6882 32.8747 26.8729 32.875 25V23.375H33.6875C33.903 23.375 34.1097 23.2894 34.262 23.137C34.4144 22.9847 34.5 22.778 34.5 22.5625V20.9375C34.5 20.722 34.4144 20.5153 34.262 20.363C34.1097 20.2106 33.903 20.125 33.6875 20.125ZM21.5 13.625C21.5 13.194 21.3288 12.7807 21.024 12.476C20.7193 12.1712 20.306 12 19.875 12C19.444 12 19.0307 12.1712 18.726 12.476C18.4212 12.7807 18.25 13.194 18.25 13.625V18.5H21.5V13.625Z"
+            fill="#0E9A6F"
+          />
         </svg>
       </div>
       <div class="terms-title">Connect Wallet</div>
@@ -463,10 +476,13 @@
       </div>
       <div class="progress">
         <div class="progress-indicator-selected" />
-        <div class="progress-indicator" />
+        <div
+          class="progress-indicator"
+          on:click={() => (showConnection = true)}
+        />
       </div>
       <button on:click={() => (showConnection = true)}>Ok, let's connect</button
       >
-    </Modal>
-  {/if}
+    {/if}
+  </Modal>
 {/if}
