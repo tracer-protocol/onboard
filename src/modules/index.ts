dc8940a0919c1fcd9f515402f68b152b6dce67e6
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-09-16 09:03:40 +1000

    Update FAQ link

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -9,7 +9,7 @@ import {
 const defaultHeading = 'Select a Wallet'
 const defaultDescription = 'Please select a wallet to connect to this dapp:'
 const defaultWalletExplanation = `Wallets are used to send, receive, and store digital assets like Ether. Wallets come in many forms. They are either built into your browser, an extension added to your browser, a piece of hardware plugged into your computer or even an app on your phone. For more information about wallets, see <a style="color: #4a90e2; font-size: 0.889rem; font-family: inherit;" class="bn-onboard-clickable" href="https://docs.ethhub.io/using-ethereum/wallets/intro-to-ethereum-wallets/" target="_blank" rel="noopener noreferrer">this explanation</a>.`
-const defaultGetHelpLink = 'https://docs.tracer.finance/faqs/tracer-faq'
+const defaultGetHelpLink = 'https://docs.tracer.finance/tracer-faqs/tracer-faqs'
 
 export default function initializeModules(
   networkId: number,
